369e1b0d497b5e610414a7ced2f267b65f5f26cb
georgejeswin <jeswinmyladoor@gmail.com>
2020-11-28 03:03:59 +0530

    deploy

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -9,8 +9,22 @@ const Popup = ({ selected, closePopup }) => {
         </h2>
         <p className="rating">Rating : {selected.imdbRating}</p>
         <div className="plot">
-          <img src={selected.Poster} alt={selected.Title} />
-          <p>{selected.Plot}</p>
+          <img
+            className="result-img"
+            src={selected.Poster}
+            alt={selected.Title}
+          />
+          <p>
+            {selected.Plot}
+            <p>Released Year : {selected.Year}</p>
+            <p>
+              IMDB Rating : <i class="far fa-star"></i>
+              {selected.imdbRating}
+            </p>
+            <p>Genre : {selected.Genre}</p>
+            <p>Runtime : {selected.Runtime}</p>
+            <p>Director : {selected.Director}</p>
+          </p>
         </div>
         <button className="close" onClick={closePopup}>
           Close
